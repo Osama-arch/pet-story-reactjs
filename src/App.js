40d256d3de20790dd2452b908e4cf3f7825bd663
Osama-arch <osamarchitectinfo@gmail.com>
2023-02-10 17:53:27 +0300

    Feat: Add jsx pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Donate from './pages/Donate';
+import Map from './pages/Map';
+import Zoos from './pages/Zoos';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<About />} />
+        <Route path='/Contact' element={<Contact />} />
+        <Route path='/Donate' element={<Donate />} />
+        <Route path='/Map' element={<Map />} />
+        <Route path='/Zoos' element={<Zoos />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Zoos.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Zoos() {
+  return <div>Zoos</div>;
+}
+
+export default Zoos;
